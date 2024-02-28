4e670d161ef33b1346f8832a9630436d344de64e
Sayakdutt <sayakofficial01@gmail.com>
2024-02-29 00:38:38 +0530

    added outlet

--- src/pages/Crypto.jsx
+++ src/pages/Crypto.jsx
@@ -1,11 +1,13 @@
 import Filter from "../components/Filter"
 import Table from "../components/Table"
+import {Outlet} from "react-router-dom";
 
 const Crypto = () => {
   return (
     <section className="xs:w-[80%] w-[90%]  h-full flex flex-col  mb-24 lg:mt-16 mt-8 relative">
       <Filter />
       <Table />
+      <Outlet />
     </section>
   )
 }
